f2b15b9678f11b2080aca09e54451415899be43e
RafaelMarques <rafammarques21@gmail.com>
2025-05-05 19:41:40 -0300

    .

--- PagesProjeto/PageConfig/MyApplication/app/src/main/java/br/com/fecapccp/myapplication/Activities/ConfigActivity.java
+++ PagesProjeto/PageConfig/MyApplication/app/src/main/java/br/com/fecapccp/myapplication/Activities/ConfigActivity.java
@@ -41,6 +41,8 @@ public class ConfigActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        carregarDadosUsuario();
+
 
         // Botão voltar
         botaoVoltar = findViewById(R.id.botaoVoltar);
